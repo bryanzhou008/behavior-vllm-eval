9be00b943ab4260593bd7de391ae9708e2f8718c
Kangrui Wang <krwang2022@gmail.com>
2024-04-09 13:37:31 -0500

    add motion planner

---
+++ igibson/motion_planner/motion_planner.py
@@ -0,0 +1,23 @@
+
+class MotionPlanner:
+    def __init__(self,activity, activity_id):
+        pass
+
+    def get_initial_state(self):
+        # return initial state of of a task scene
+        pass
+
+    def get_goal_condition(self):
+        # return goal condition of a task
+        pass
+    
+    @staticmethod
+    def get_relavant_objects(demo):
+        # return relevant objects in the scene
+        # three methods to get relavnt objects
+        # objects included in the goal condition/ diff between initial and goal state
+        # objects touched by the agent
+        # objects gazed at in a significant amount of time
+        pass
+
+
--- igibson/transition_model/transition_model.py
+++ igibson/transition_model/transition_model.py
@@ -3,35 +3,16 @@ class Transition_model:
         pass
     
 
-    def get_simulator_state(self):
+    def get_simulator_state(self,simulator):
         # convert simulator state to a dictionary
         pass
 
-    def apply_action(self,action):
+    def apply_action(self,action,simulator):
         # apply action to simulator and return the next state
         pass
 
     
 
-class MotionPlanner:
-    def __init__(self,activity, activity_id):
-        pass
 
-    def get_initial_state(self):
-        # return initial state of of a task scene
-        pass
-
-    def get_goal_condition(self):
-        # return goal condition of a task
-        pass
-    
-    @staticmethod
-    def get_relavant_objects(demo):
-        # return relevant objects in the scene
-        # three methods to get relavnt objects
-        # objects included in the goal condition/ diff between initial and goal state
-        # objects touched by the agent
-        # objects gazed at in a significant amount of time
-        pass
 
 
