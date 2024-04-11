e4a7259224ab39808b7d0c2696920517f76f6eb9
Yu Zhou <bryanzhou008@ucla.edu>
2024-04-10 20:25:07 -0700

    added motion planner simple version

--- igibson/motion_planner/motion_planner.py
+++ igibson/motion_planner/motion_planner.py
@@ -1,23 +1,73 @@
+import bddl
+from bddl.activity import Conditions, get_initial_conditions, get_goal_conditions, get_object_scope
+# from bddl.activity import *
+import json
+
 
 class MotionPlanner:
-    def __init__(self,activity, activity_id):
-        pass
+    def __init__(self, 
+                 behavior_activity, 
+                 activity_definition, 
+                 simulator_name):
+        self.behavior_activity = behavior_activity 
+        self.activity_definition = activity_definition
+        self.simulator_name = simulator_name
+        
+        self.conditions = Conditions(behavior_activity, activity_definition, simulator_name)
+        self.conditions.object_scope = get_object_scope(self.conditions)
+
 
     def get_initial_state(self):
-        # return initial state of of a task scene
-        pass
+        """
+        Returns a JSON string containing human-readable and machine-readable initial states of the activity.
+        """
+        
+        initial_state_data = {
+            "human_readable": self.conditions.parsed_initial_conditions,
+            "machine_readable": get_initial_conditions(self.conditions)
+        }
 
+        return json.dumps(initial_state_data, indent=4)
+
+        
+        
     def get_goal_condition(self):
-        # return goal condition of a task
-        pass
+        """
+        Returns a JSON string containing human-readable and machine-readable goal conditions of the activity.
+        """
+        goal_condition_data = {
+            "human_readable": self.conditions.parsed_goal_conditions,
+            "machine_readable": get_goal_conditions(self.conditions, self.conditions.object_scope)
+        }
+
+        return json.dumps(goal_condition_data, indent=4)
+    
     
     @staticmethod
-    def get_relavant_objects(demo):
+    def bddl_get_relavant_objects(self):
+        """
+        Returns a JSON string containing relevant objects from bddl definition, key is object type and value includes all relevant instances of that object type
+        """
+        relavant_objects = self.conditions.parsed_objects
+        
+        return json.dumps(relavant_objects, indent=4)
+        
+        
+        
+    
+    
+    
+    
+    @staticmethod
+    def igibson_get_relavant_objects(self, demo):
         # return relevant objects in the scene
         # three methods to get relavnt objects
         # objects included in the goal condition/ diff between initial and goal state
         # objects touched by the agent
         # objects gazed at in a significant amount of time
+        
+        
+        
         pass
 
 
